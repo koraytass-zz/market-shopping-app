17ee83be451ceb23eb08436e2f01d017ad31af1e
KorayTas <koraytass@gmail.com>
2021-09-28 23:11:50 +0300

    typo mistakes corrected in App.tsx, fethingCompanies action addded

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,21 @@ import logo from "./logo.svg";
 import "./App.css";
 import { getCompanies, getProducts } from "./utils/dbManagement";
 import { useDispatch } from "react-redux";
+import { fetchingCompanies, fetchingProducts } from "./redux/actions";
 
 function App() {
   const dispatch = useDispatch();
   useEffect(() => {
     const getProductsWrapper = async () => {
       const products = await getProducts();
-      dispatch({type: 'FETCHING_PRODUCTS', products: products.dbProducts});
+      dispatch(fetchingProducts(products.dbProducts));
+    };
+    const getCompaniesWrapper = async () => {
+      const companies = await getCompanies();
+      dispatch(fetchingCompanies(companies.dbCompanies));
     };
     getProductsWrapper();
+    getCompaniesWrapper();
   }, []);
   return (
     <div className="App">
