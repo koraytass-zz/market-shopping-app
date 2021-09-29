a8b6e0014f68b534585e574ea93b60d5cde812d7
KorayTas <koraytass@gmail.com>
2021-09-30 00:10:20 +0300

    filtering by brands property added

--- src/components/searchingByBrands.tsx
+++ src/components/searchingByBrands.tsx
@@ -1,24 +1,56 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Checkbox from "./checkBox";
 import SearchInput from "./searchInput";
+import styled from "styled-components";
+import { getBrands } from "../utils/dbManagement";
 
 const SearchingByBrands = () => {
-  const [checkedBoxes, setCheckedBoxes] = useState({});
+  const [brands, setBrands] = useState<string[]>();
+  const [checkedBoxes, setCheckedBoxes] = useState<{ [key: string]: boolean }>(
+    {}
+  );
   const handleCheckboxChange: (event: any) => void = (event) => {
     setCheckedBoxes({
       ...checkedBoxes,
       [event.target.name]: event.target.checked,
     });
   };
-  return (
-    <>
+
+  useEffect(() => {
+    const getTagsWrapper = async () => {
+      let dbBrands: string[] = await getBrands();
+      setBrands(dbBrands);
+    };
+    getTagsWrapper();
+  }, []);
+
+  const Container = styled.div`
+  height: 214px;
+  width: 296px;
+`;
+const TagContainer = styled.div`
+  overflow: auto;
+  height:99px;
+  width:142px;
+`;
+return (
+  <>
+    <Container>
       <SearchInput searchType={"brand"}></SearchInput>
-{/*       <Checkbox
-        checked={checkedBoxes[]}
-        onChange={handleCheckboxChange}
-        name={}
-      /> */}
-    </>
-  );
+      <TagContainer>
+      {brands &&
+        brands.map((brandName: string) => {
+          return (
+            <Checkbox
+              name={brandName}
+              checked={checkedBoxes[brandName]}
+              onChange={handleCheckboxChange}
+            />
+          );
+        })}
+        </TagContainer>
+    </Container>
+  </>
+);
 };
 export default SearchingByBrands;
