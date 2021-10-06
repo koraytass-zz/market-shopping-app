d949d2a666e9ca40b09dd0eb6c2f1e4241537cd1
KorayTas <koraytass@gmail.com>
2021-10-06 13:26:31 +0300

    target name   update

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,6 +2,6 @@ const createProxyMiddleware = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    createProxyMiddleware(["/dbProducts", "/dbCompanies"], { target: "http://localhost:3004" })
+    createProxyMiddleware(["/dbProducts", "/dbCompanies"], { target: "https://case-study-koray.herokuapp.com" })
   );
 };
